7a838d4a576142f3ff111c64d4ed0de94531d9da
infinity-Noah <nikhil950833@gmail.com>
2026-09-13 00:19:42 +0530

    meRouter-added

---
+++ backend/src/lib/users.ts
@@ -0,0 +1,12 @@
+//This function basically takes the user id given to if
+//no need to check cause checking is already done in meRouter from clerk side getAuth()
+//this is just to query the user id from the db and return rest of the information
+
+import { eq } from "drizzle-orm";
+import { db } from "../db/index.js";
+import { users } from "../db/schema";
+
+export async function getLocalUser(clerkUserId:string) {
+    const [row] = await db.select().from(users).where(eq(users.clerkUserId, clerkUserId)).limit(1);
+    return row;
+}
