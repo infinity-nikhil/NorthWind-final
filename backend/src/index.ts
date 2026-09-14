a432735567d5e2623d13f434a7306e54b8754604
infinity-Noah <nikhil950833@gmail.com>
2026-09-14 16:50:41 +0530

    checkout-controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import { getEnv } from "./lib/env";
 import meRouter from "./routes/meRouter"; 
 import productRouter from "./routes/productRouter";
 import streamRouter from "./routes/streamRoutes";
+import checkoutRouter from "./routes/checkoutRouter";
 
 const env = getEnv();
 const app = express();
@@ -20,12 +21,18 @@ app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res);
 });
 
+//for the next section
+// app.post("/webhooks/polar", rawJson, (req, res) => {
+//   void polarWebhookHandler(req, res);
+// });
+
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.use("api/me", meRouter);
-app.use("api/products", productRouter);
+app.use("/api/me", meRouter);
+app.use("/api/products", productRouter);
 app.use("/api/stream", streamRouter);
+app.use("/api/checkout", checkoutRouter);
 
 app.listen(env.PORT, () => console.log("listening on port:", env.PORT));
